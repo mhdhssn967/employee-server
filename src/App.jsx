357934f3531b6fb7560e8c3daf93dd9c8f03e028
mhdhssn967 <mhdhssn967@gmail.com>
2024-11-15 17:36:43 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Add from './components/Add.jsx'
+import Display from './components/Display.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Add/>
+      <Display/>
+    </div>
+  )
+}
+
+export default App
